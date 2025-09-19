fee8ce62543e1cdf60e86c9694cfc70cc6110c05
Thembile Maseko <thembilemaseko78@gmail.com>
2025-09-19 20:34:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Welcome to AI Poem Generator");
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+        strings: ["Code whispers secrets, lines of might"],
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+let poemFormElement = document.querySelector("form");
+poemFormElement.addEventListener("submit", generatePoem);
